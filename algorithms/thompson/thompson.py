0b10ba76b2e8dbcce1f65b789f6afa888d458b9e
ianpan870102 <ianyepan@outlook.com>
2020-10-06 10:40:54 +0800

    Create template file for TS

---
+++ algorithms/thompson/thompson.py
@@ -0,0 +1,21 @@
+def argmax(arr):
+  return arr.index(max(arr))
+
+
+class ThompsonSampling():
+  def __init__(self):
+    pass
+
+  def initialize(self):
+    pass
+
+  def get_name(self):
+    """Returns the name of this algorithm"""
+    return "Thompson Sampling"
+
+  def select_arm(self) -> int:
+    """Returns the chosen arm's index (0-based)."""
+    pass
+
+  def update(self, chosen_arm, latest_reward):
+    pass
